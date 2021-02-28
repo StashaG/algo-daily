cb6ac21d55382e7d85803dce22a4b324b5257179
Stacey Graham <stacey.graham1106@gmail.com>
2021-02-28 17:17:57 -0500

    adding reverseString file and reverseString problem

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+/*Write a function that reverses an inputted string without using the 
+built-in Array#reverse method?*/
+
+
+function reverseString(str) {
+    // reverse str
+    
+    return str;
+  }
