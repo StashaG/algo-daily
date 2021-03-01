bfe03c1aa99fe6dd5a3ec75e0a3fe967a4fd1825
Stacey Graham <stacey.graham1106@gmail.com>
2021-02-28 19:32:56 -0500

    adding my solution to reverseString problem

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,16 @@
 /*Write a function that reverses an inputted string without using the 
 built-in Array#reverse method?*/
 
+//Solution
 
 function reverseString(str) {
     // reverse str
-    
-    return str;
+    let newString = " ";
+
+    for (let i = str.length-1; i >= 0; i--) { //start from the end of the string and move the pointer down from the beginning
+        newString += str[i];
+    }
+    return newString;
   }
+
+  console.log(reverseString("love"));
