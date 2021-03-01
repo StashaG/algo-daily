41885c34787447a155a1d839de463911ef05bdc2
Stacey Graham <stacey.graham1106@gmail.com>
2021-02-28 19:55:24 -0500

    adding my solution to reverseString problem using two pointer technique

--- reverseString.js
+++ reverseString.js
@@ -3,7 +3,7 @@ built-in Array#reverse method?*/
 
 //Solution
 
-function reverseString(str) {
+/*function reverseString(str) {
     // reverse str
     let newString = " ";
 
@@ -13,4 +13,23 @@ function reverseString(str) {
     return newString;
   }
 
+  console.log(reverseString("love"));*/
+
+  //Solution using the Two Pointer Technique
+  
+  function reverseString(str) {
+    let strArr = str.split(""); //splits string "love" into an array of substrings
+    let start = 0;
+    let end = str.length - 1;
+  
+    while (start <= end) {
+      const temp = strArr[start];
+      strArr[start] = strArr[end];
+      strArr[end] = temp;
+      start++;
+      end--;
+    }
+  
+    return strArr.join("");
+  }
   console.log(reverseString("love"));
